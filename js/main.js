8e3f37a56c37fdf832bb6be11b077d86e8f86528
aChineseBoyJimLi <794486346@qq.com>
2017-11-17 15:18:30 +0800

    origin

---
+++ js/main.js
@@ -0,0 +1,55 @@
+function main() {
+
+(function () {
+
+// 循环轮播到上一个项目
+      $("#myCarousel").carousel('cycle');
+      $("#prev-btn").click(function(){
+         $("#myCarousel").carousel('prev');
+      });
+      // 循环轮播到下一个项目
+      $("#next-btn").click(function(){
+         $("#myCarousel").carousel('next');
+      });
+   /* ==============================================
+  	Testimonial Slider
+  	=============================================== */ 
+
+  	$('a.page-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - 50
+            }, 900);
+            return false;
+          }
+        }
+      });
+
+    /*====================================
+    Show Menu on Book
+    ======================================*/
+    $(window).bind('scroll', function() {
+        var navHeight = $(window).height() - 60;
+        if ($(window).scrollTop() > navHeight) {
+            $('.navbar-default').addClass('on');
+        } else {
+            $('.navbar-default').removeClass('on');
+        }
+    });
+
+    $('body').scrollspy({ 
+        target: '.navbar-default',
+        offset: 80
+    })
+
+  	$(document).ready(
+        function(){
+
+        }
+    )
+}());
+}
+main();
